be061b3747aeae4ebd72b2fa1dd5fc2c9e2ae027
Pavel Ya <pavelya1c@gmail.com>
2025-11-29 13:48:06 +0400

    dailyCommit

--- src/main/java/pavel/Main.java
+++ src/main/java/pavel/Main.java
@@ -2,6 +2,6 @@ package pavel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, qa_guru_git_39!");
+        System.out.println("Hello, qa_guru_git_39test!");
     }
 }
