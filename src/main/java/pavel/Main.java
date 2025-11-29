454439dcc4ec39b86815c999d41e307564d06a65
Pavel Ya <pavelya1c@gmail.com>
2025-11-29 14:36:54 +0400

    add new massage

--- src/main/java/pavel/Main.java
+++ src/main/java/pavel/Main.java
@@ -3,8 +3,7 @@ package pavel;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, qa_guru_git_39test_4_get_pull!");
+        System.out.println("Hello, qa_guru_git_39test_4_get_p2ull!");
+
     }
-    private static String getMessage "gg";
-return
-        "Hello qa-guru";
 }
