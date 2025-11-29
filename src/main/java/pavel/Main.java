f4a0478b29740b7da9f9765ee9e10359a0845746
Pavel Ya <pavelya1c@gmail.com>
2025-11-29 13:52:31 +0400

    new-branch_merge

--- src/main/java/pavel/Main.java
+++ src/main/java/pavel/Main.java
@@ -2,6 +2,6 @@ package pavel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, qa_guru!");
+        System.out.println("Hello, qa_guru_branch_merge_test!");
     }
 }
