6db91040ef510027866e1bdb1f5aa830813694fd
pavelya1c <pavelya1c@gmail.com>
2025-11-29 14:34:04 +0400

    Update Main.java

--- src/main/java/pavel/Main.java
+++ src/main/java/pavel/Main.java
@@ -2,6 +2,6 @@ package pavel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, qa_guru_git_39test_4_get_pull!");
+        System.out.println("Hello, qa_guru_git_39test_4_get_pull!!!!!!");
     }
 }
