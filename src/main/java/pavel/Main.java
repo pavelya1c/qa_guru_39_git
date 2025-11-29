67204926ca7d79f39dbb9228ae13865e13be00ad
Pavel Ya <pavelya1c@gmail.com>
2025-11-29 14:55:02 +0400

    newChangecom1

--- src/main/java/pavel/Main.java
+++ src/main/java/pavel/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, qa_guru_branch_merge_test!");
         System.out.println("Hello, qa_guru_branch_merge_test!");
-        System.out.println("Hello, qa_guru_branch_merge_test!");
+        System.out.println("Hello, qa_guru_branch_m234234erge_test!");
         System.out.println("Hello, qa_guru_branch_merge_test!");
         System.out.println("Hello, qa_guru_git_39test_4_get_pull!");
         System.out.println("Hello, qa_guru_git_39test_4_get_p2ull!");
