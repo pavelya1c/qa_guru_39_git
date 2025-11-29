1d8d93eb758721b69e4d4f35cda1fa1a88daeb89
Pavel Ya <pavelya1c@gmail.com>
2025-11-29 14:58:50 +0400

    1banch

--- src/main/java/pavel/Main.java
+++ src/main/java/pavel/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("Hello, qa_guru_branch_merge_test!");
         System.out.println("Hello, qa_guru_brrwerwanch_m234234erge_test!");
         System.out.println("Hello, qa_guru_branch_merge_test!");
-        System.out.println("Hello, qa_gurqrru_git_39test_4_get_pull!");
+        System.out.println("Hello, qa_gurqrru_git_qweqwe39test_4_get_pull!");
         System.out.println("Hello, qa_guru_git_39test_4_get_p2ull!");
         //19;//19///123
     }
