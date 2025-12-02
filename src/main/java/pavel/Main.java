b0ee45d596ba3f834f744eeec0f529a056c38544
Pavel Ya <pavelya1c@gmail.com>
2025-12-02 13:57:03 +0400

    initialCommit

--- src/main/java/pavel/Main.java
+++ src/main/java/pavel/Main.java
@@ -3,11 +3,6 @@ package pavel;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, qa_guru_branch_merge_test!");
-        System.out.println("Hello, qa_guru_branch_merge_test!");
-        System.out.println("Hello, qa_guru_branch_m234234erge_test!");
-        System.out.println("Hello, qa_guru_branch_merge_test!");
-        System.out.println("Hello, qa_guru_git_39test_4_get_pull!");
-        System.out.println("Hello, qa_guru_git_39test_4_get_p2ull!");
- //19;//19///123
+      
     }
 }
